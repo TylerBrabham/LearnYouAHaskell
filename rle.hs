1d6d40b08156cefbd3ca8b3ec84095a041660583
TylerBrabham <tylerbrabham@berkeley.edu>
2014-11-05 19:12:28 -0800

    Update rle.hs

--- rle.hs
+++ rle.hs
@@ -1,4 +1,3 @@
-
 rleHelper :: [(Char, Char)] -> Int -> String -> String
 rleHelper [] _ outputString = outputString
 rleHelper ((a, b) : ys) count outputString
@@ -6,6 +5,9 @@ rleHelper ((a, b) : ys) count outputString
   | otherwise = rleHelper ys 1 updatedOutputString
     where updatedOutputString = outputString ++ [a] ++ (show count)
 
+{- 
+  Function to compute the run length encoding of a given string x.
+-}
 rle :: String -> String
 rle x = rleHelper y 1 ""
   where
